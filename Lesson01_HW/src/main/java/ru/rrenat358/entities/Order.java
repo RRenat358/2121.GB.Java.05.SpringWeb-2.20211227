67fb6689f6d551789ee1843282fc9d1d884c4c92
rrenat358 <rrenat358@gmail.com>
2023-06-01 17:54:28 +0300

    1754

--- Lesson01_HW/src/main/java/ru/rrenat358/entities/Order.java
+++ Lesson01_HW/src/main/java/ru/rrenat358/entities/Order.java
@@ -19,7 +19,6 @@ import java.util.List;
 @AllArgsConstructor
 public class Order {
 
-
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "id")
@@ -38,73 +37,6 @@ public class Order {
     @Column(name = "phone")
     private int phone;
 
-
-
-
-
-    private List<OrderItemDto> itemList;
-    private int totalPrice;
-
-//    public Order() {
-//        this.itemList = new ArrayList<>();
-//    }
-
-
-    public void addProduct(Product product) {
-        if (addProduct(product.getId())) {
-            return;
-        }
-        itemList.add(new OrderItemDto(product));
-        recalculate();
-    }
-
-
-    public boolean addProduct(Long id) {
-        for (OrderItemDto orderItemDto : itemList) {
-            if (orderItemDto.getProductId().equals(id)) {
-                orderItemDto.changeQuantity(+1);
-                recalculate();
-                return true;
-            }
-        }
-        return false;
-    }
-
-
-    public void decreaseProduct(Long id) {
-        Iterator<OrderItemDto> iter = itemList.iterator();
-        while (iter.hasNext()) {
-            OrderItemDto orderItemDto = iter.next();
-            if (orderItemDto.getProductId().equals(id)) {
-                orderItemDto.changeQuantity(-1);
-                if (orderItemDto.getQuantity() <= 0) {
-                    iter.remove();
-                }
-                recalculate();
-                return;
-            }
-        }
-    }
-
-
-    public void removeProduct(Long id) {
-        itemList.removeIf(orderItemDto -> orderItemDto.getProductId().equals(id));
-        recalculate();
-    }
-
-
-    private void recalculate() {
-        totalPrice = 0;
-        for (OrderItemDto orderItemDto : itemList) {
-            totalPrice += orderItemDto.getPrice();
-        }
-    }
-
-
-    public void clear() {
-        itemList.clear();
-        totalPrice = 0;
-    }
 
 
 }
