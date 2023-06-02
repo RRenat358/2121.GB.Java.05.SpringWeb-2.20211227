ede3e4e9de2cb230b886441cbda4e2d62784c6c1
rrenat358 <rrenat358@gmail.com>
2023-06-02 16:23:56 +0300

    1623

--- Lesson01_HW/src/main/java/ru/rrenat358/services/OrderItemService.java
+++ Lesson01_HW/src/main/java/ru/rrenat358/services/OrderItemService.java
@@ -2,17 +2,31 @@ package ru.rrenat358.services;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import ru.rrenat358.dto.Cart;
+import ru.rrenat358.dto.OrderItemDto;
 import ru.rrenat358.entities.OrderItem;
+import ru.rrenat358.repositories.OrderItemsRepository;
+
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
 public class OrderItemService {
 
     private final Cart cart;
+    private final CartService cartService;
     private final OrderItem orderItem;
+    private final OrderItemsRepository orderItemsRepository;
 
 
+
+    @Transactional
+    public void save() {
+        List<OrderItemDto> orderItems = cartService.getCurrentCart().getItemList();
+        orderItemsRepository.save(orderItem);
+
+    }
 
 
 
