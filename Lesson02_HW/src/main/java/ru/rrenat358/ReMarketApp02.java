8aff31accb38ffbc0164bd96cd11ce810ab18932
rrenat358 <rrenat358@gmail.com>
2023-06-03 09:38:07 +0300

    0938

---
+++ Lesson02_HW/src/main/java/ru/rrenat358/ReMarketApp02.java
@@ -0,0 +1,16 @@
+package ru.rrenat358;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReMarketApp02 {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(ReMarketApp02.class, args);
+
+    }
+
+
+}
