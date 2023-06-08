269186b0c76cec60db50a6419b134a81df097e8e
rrenat358 <rrenat358@gmail.com>
2023-06-08 14:03:56 +0300

    1403

---
+++ Lesson04-02-SOAP/src/main/java/ru/rrenat358/endpoints/StudentEndpoint.java
@@ -0,0 +1,48 @@
+package ru.rrenat358.endpoints;
+
+import ru.rrenat358.services.StudentService;
+import ru.rrenat358.soap.students.GetAllStudentsRequest;
+import ru.rrenat358.soap.students.GetAllStudentsResponse;
+import ru.rrenat358.soap.students.GetStudentByNameRequest;
+import ru.rrenat358.soap.students.GetStudentByNameResponse;
+import ru.rrenat358.soap.students.*;
+import lombok.RequiredArgsConstructor;
+import org.springframework.ws.server.endpoint.annotation.Endpoint;
+import org.springframework.ws.server.endpoint.annotation.PayloadRoot;
+import org.springframework.ws.server.endpoint.annotation.RequestPayload;
+import org.springframework.ws.server.endpoint.annotation.ResponsePayload;
+
+@Endpoint
+@RequiredArgsConstructor
+public class StudentEndpoint {
+    private static final String NAMESPACE_URI = "http://www.flamexander.com/spring/ws/students";
+    private final StudentService studentService;
+
+    @PayloadRoot(namespace = NAMESPACE_URI, localPart = "getStudentByNameRequest")
+    @ResponsePayload
+    public GetStudentByNameResponse getStudentByName(@RequestPayload GetStudentByNameRequest request) {
+        GetStudentByNameResponse response = new GetStudentByNameResponse();
+        response.setStudent(studentService.getByName(request.getName()));
+        return response;
+    }
+
+    /*
+        Пример запроса: POST http://localhost:8080/ws
+        Header -> Content-Type: text/xml
+
+        <soapenv:Envelope xmlns:soapenv="http://schemas.xmlsoap.org/soap/envelope/" xmlns:f="http://www.flamexander.com/spring/ws/students">
+            <soapenv:Header/>
+            <soapenv:Body>
+                <f:getAllStudentsRequest/>
+            </soapenv:Body>
+        </soapenv:Envelope>
+     */
+
+    @PayloadRoot(namespace = NAMESPACE_URI, localPart = "getAllStudentsRequest")
+    @ResponsePayload
+    public GetAllStudentsResponse getAllStudents(@RequestPayload GetAllStudentsRequest request) {
+        GetAllStudentsResponse response = new GetAllStudentsResponse();
+        studentService.getAllStudents().forEach(response.getStudents()::add);
+        return response;
+    }
+}
---
+++ Lesson04-02-SOAP/src/main/java/ru/rrenat358/services/GroupService.java
@@ -0,0 +1,26 @@
+package ru.rrenat358.services;
+
+import ru.rrenat358.repositories.GroupRepository;
+import ru.rrenat358.entities.GroupEntity;
+import ru.rrenat358.soap.groups.Group;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.function.Function;
+
+@Service
+@RequiredArgsConstructor
+public class GroupService {
+    private final GroupRepository groupRepository;
+
+    public static final Function<GroupEntity, Group> functionEntityToSoap = ge -> {
+        Group g = new Group();
+        g.setTitle(g.getTitle());
+        ge.getStudents().stream().map(StudentService.functionEntityToSoap).forEach(s -> g.getStudents().add(s));
+        return g;
+    };
+
+    public Group getByTitle(String title) {
+        return groupRepository.findByTitle(title).map(functionEntityToSoap).get();
+    }
+}
