2d7c3a7d1883eb76f709e197d39fcf5460fb245d
rrenat358 <rrenat358@gmail.com>
2023-06-11 14:46:20 +0300

    1446

---
+++ Lesson06_HW/spring-web/core-service/src/main/java/com/geekbrains/spring/web/core/dto/OrderItemDto.java
@@ -0,0 +1,30 @@
+package com.geekbrains.spring.web.core.dto;
+
+import com.geekbrains.spring.web.core.entities.Product;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class OrderItemDto {
+    private Long productId;
+    private String productTitle;
+    private int quantity;
+    private int pricePerProduct;
+    private int price;
+
+    public OrderItemDto(Product product) {
+        this.productId = product.getId();
+        this.productTitle = product.getTitle();
+        this.quantity = 1;
+        this.pricePerProduct = product.getPrice();
+        this.price = product.getPrice();
+    }
+
+    public void changeQuantity(int delta) {
+        this.quantity += delta;
+        this.price = this.quantity * this.pricePerProduct;
+    }
+}
