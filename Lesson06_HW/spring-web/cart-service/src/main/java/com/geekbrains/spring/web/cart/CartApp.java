9934219267dbddb1e6b236b7a75f8fb0334e82a8
rrenat358 <rrenat358@gmail.com>
2023-06-12 10:27:09 +0300

    1027

---
+++ Lesson06_HW/spring-web/cart-service/src/main/java/com/geekbrains/spring/web/cart/CartApp.java
@@ -0,0 +1,12 @@
+package com.geekbrains.spring.web.cart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CartApp {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CartApp.class, args);
+	}
+}
