b36b99656c6e7d68dd5ed91c5331b5c6f01b6cbb
rrenat358 <rrenat358@gmail.com>
2023-06-14 21:41:04 +0300

    2140

---
+++ Lesson08-Integration/spring-web/cart-service/src/main/java/com/geekbrains/spring/web/cart/exceptions/GlobalExceptionHandler.java
@@ -0,0 +1,19 @@
+package com.geekbrains.spring.web.cart.exceptions;
+
+import com.geekbrains.spring.web.api.exceptions.AppError;
+import com.geekbrains.spring.web.api.exceptions.ResourceNotFoundException;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
+@Slf4j
+public class GlobalExceptionHandler {
+    @ExceptionHandler
+    public ResponseEntity<AppError> catchResourceNotFoundException(ResourceNotFoundException e) {
+        log.error(e.getMessage(), e);
+        return new ResponseEntity<>(new AppError(HttpStatus.NOT_FOUND.value(), e.getMessage()), HttpStatus.NOT_FOUND);
+    }
+}
---
+++ Lesson08-Integration/spring-web/cart-service/src/main/java/com/geekbrains/spring/web/cart/models/Cart.java
@@ -0,0 +1,87 @@
+package com.geekbrains.spring.web.cart.models;
+
+import com.geekbrains.spring.web.api.core.ProductDto;
+import lombok.Data;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+@Data
+public class Cart {
+    private List<CartItem> items;
+    private int totalPrice;
+
+    public Cart() {
+        this.items = new ArrayList<>();
+    }
+
+    public void add(ProductDto productDto) {
+        if (add(productDto.getId())) {
+            return;
+        }
+        items.add(new CartItem(productDto));
+        recalculate();
+    }
+
+    public boolean add(Long id) {
+        for (CartItem o : items) {
+            if (o.getProductId().equals(id)) {
+                o.changeQuantity(1);
+                recalculate();
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public void decrement(Long productId) {
+        Iterator<CartItem> iter = items.iterator();
+        while (iter.hasNext()) {
+            CartItem o = iter.next();
+            if (o.getProductId().equals(productId)) {
+                o.changeQuantity(-1);
+                if (o.getQuantity() <= 0) {
+                    iter.remove();
+                }
+                recalculate();
+                return;
+            }
+        }
+    }
+
+    public void remove(Long productId) {
+        items.removeIf(o -> o.getProductId().equals(productId));
+        recalculate();
+    }
+
+    public void clear() {
+        items.clear();
+        totalPrice = 0;
+    }
+
+    private void recalculate() {
+        totalPrice = 0;
+        for (CartItem o : items) {
+            totalPrice += o.getPrice();
+        }
+    }
+
+    public void merge(Cart another) {
+        for (CartItem anotherItem : another.items) {
+            boolean merged = false;
+            for (CartItem myItem : items) {
+                if (myItem.getProductId().equals(anotherItem.getProductId())) {
+                    myItem.changeQuantity(anotherItem.getQuantity());
+                    merged = true;
+                    break;
+                }
+            }
+            if (!merged) {
+                items.add(anotherItem);
+            }
+        }
+        recalculate();
+        another.clear();
+    }
+}
